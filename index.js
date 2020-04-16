dd0b4283994c1f9e55723f7cbf4e3a86e9994dcb
donkey96 <noire96@gmail.com>
2020-04-16 19:08:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var ejs = require("ejs"); // ★追加
+
+var app = express();
+
+app.engine('ejs', ejs.renderFile); // ★追加
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Express!')
-})
+  // index.ejsをレンダリングする
+  res.render('index.ejs',
+    {title: 'Index',
+      content: 'This is Express-app Top page!'});
+});
 
-app.listen(3000, () => {
-  console.log('Start server port:3000')
+var server = app.listen(3000, () => {
+  console.log('Server is running');
 })

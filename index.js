3a96e3eae80fa8b80d904bc1f53e98d30d7a194f
donkey96 <[noire964@gmail.com]>
2020-05-21 19:49:32 +0900

    メインプログラムの修正

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ app.engine('ejs', ejs.renderFile);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  var msg = 'This is Express page!<br>' + 'これは、スタイルシートを利用した例です。';
+  var msg = 'This is Index page!<br>' + 'これは、トップページです。';
   // index.ejsをレンダリングする
   res.render('index.ejs', {
     title: 'Index',
-    content: msg
+    content: msg,
+    link: {href: '/other', text: '※別のページに移動'}
+  });
+});
+
+app.get('/other', (req, res) => {
+  var msg = 'This is Other Page!<br>' + 'これは、用意された別のページです。'
+  res.render('index.ejs', {
+    title: 'Other',
+    content: msg,
+    link: {href: '/', text: '※トップに戻る'}
   });
 });
 

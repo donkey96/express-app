946ae7a86bc179fb55e13e573a778db854a452de
donkey96 <[noire964@gmail.com]>
2020-05-21 19:56:40 +0900

    クエリパラメーターの送付

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   var msg = 'This is Index page!<br>' + 'これは、トップページです。';
+  var url = '/other?name=taro&pass=yamada';
   // index.ejsをレンダリングする
   res.render('index.ejs', {
     title: 'Index',
     content: msg,
-    link: {href: '/other', text: '※別のページに移動'}
+    link: {href: url, text: '※別のページに移動'}
   });
 });
 
 app.get('/other', (req, res) => {
-  var msg = 'This is Other Page!<br>' + 'これは、用意された別のページです。'
+  var name = req.query.name;
+  var pass = req.query.pass;
+  var msg = 'あなたの名前は「' + name + '」<br>パスワードは「' + pass + '」です。';
   res.render('index.ejs', {
     title: 'Other',
     content: msg,
